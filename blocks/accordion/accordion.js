8cf803bb58b255c6f55beb9eb968b21221311d0b
Vijay Rawat <vijay.rawat@iamhelium.com.au>
2025-04-08 01:17:02 +0530

    update:  accordion js

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -1,15 +1,13 @@
 export default function decorate(block) {
-  document.querySelectorAll('.accordion').forEach((accordionWrapper) => {
-    accordionWrapper.querySelectorAll('[data-aue-model="accordionitem"]').forEach((item) => {
-      const label = item.querySelector('[data-aue-prop="accordionitemlabel"]');
-      label.addEventListener('click', () => {
-        accordionWrapper.querySelectorAll('[data-aue-model="accordionitem"].open').forEach((openItem) => {
-          if (openItem !== item) {
-            openItem.classList.remove('open');
-          }
-        });
-        item.classList.toggle('open');
+  block.querySelectorAll('[data-aue-model="accordionitem"]').forEach((item) => {
+    const label = item.querySelector('[data-aue-prop="accordionitemlabel"]');
+    label.addEventListener('click', () => {
+      block.querySelectorAll('[data-aue-model="accordionitem"].open').forEach((openItem) => {
+        if (openItem !== item) {
+          openItem.classList.remove('open');
+        }
       });
+      item.classList.toggle('open');
     });
   });
 }
