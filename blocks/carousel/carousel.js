565d5aca940ef3e879c9a3d8993926a77e5ea2ea
jananivm10 <120348366+jananivm10@users.noreply.github.com>
2025-04-14 10:22:02 +1000

    Added Slide block

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -1,4 +1,3 @@
-/*
 // import { fetchPlaceholders, getMetadata } from '../../scripts/aem.js';
 
 // const placeholders = await fetchPlaceholders(getMetadata('locale'));
@@ -82,4 +81,3 @@ export default function decorate(block) {
     });
   });
 }
-*/
