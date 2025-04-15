552c8ad2804e4b773de4efd769752cb09a0791f4
Vijay Rawat <vijay.rawat@iamhelium.com.au>
2025-04-15 23:34:32 +0530

    add: article block

---
+++ blocks/article/article.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-console */
+
+import ffetch from '../../scripts/ffetch.js';
+import { cleanUrl } from '../../scripts/helper.js';
+
+export default async function decorate(block) {
+  console.log(block);
+}
