f98376fa196c1e4a425d7539a895c72faa5e100a
Vijay Rawat <vijay.rawat@iamhelium.com.au>
2025-04-23 14:19:03 +0530

    update article js

--- blocks/articles/articles.js
+++ blocks/articles/articles.js
@@ -122,9 +122,8 @@ export default async function decorate(block) {
   // child-article-tag logic
   const uid = generateUID();
   let tags = [];
-
+  console.log('TAXO: ', await ffetch('/taxonomy.json').sheet('tags').all());
   try {
-    console.log('TAXO: ', await ffetch('/taxonomy.json').sheet('tags').all());
     const taxonomy = await ffetch('/taxonomy.json').sheet('tags').all();
     console.log('taxonomy: ', taxonomy);
     tags = taxonomy.default.data || [];
