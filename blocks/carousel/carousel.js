94aa083dd8b69b3c60f7f8a1063c13a6c46c61f8
Vijay Rawat <vijay.rawat@iamhelium.com.au>
2025-04-25 21:22:46 +0530

    udpate carousel json and js

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -1,30 +1,30 @@
-import decorateTeaser from '../teaser/teaser.js';
+// import decorateTeaser from '../teaser/teaser.js';
 
-export default async function decorate(block) {
-  const rawTeasers = [...block.children].filter((childDiv) => {
-    const marker = childDiv.querySelector(':scope > div:first-child > p');
-    return marker && marker.textContent.includes('teaser');
-  });
+// export default async function decorate(block) {
+//   const rawTeasers = [...block.children].filter((childDiv) => {
+//     const marker = childDiv.querySelector(':scope > div:first-child > p');
+//     return marker && marker.textContent.includes('teaser');
+//   });
 
-  await Promise.all(rawTeasers.map(async (teaserRaw) => {
-    const rows = [...teaserRaw.children];
+//   await Promise.all(rawTeasers.map(async (teaserRaw) => {
+//     const rows = [...teaserRaw.children];
 
-    const configText = rows[0]?.querySelector('p')?.textContent.trim() || '';
-    const blockClasses = configText.split(',').map((cls) => cls.trim()).filter(Boolean);
+//     const configText = rows[0]?.querySelector('p')?.textContent.trim() || '';
+//     const blockClasses = configText.split(',').map((cls) => cls.trim()).filter(Boolean);
 
-    // Create a new block without the config row
-    const teaserBlock = document.createElement('div');
-    teaserBlock.append(...rows.slice(1));
+//     // Create a new block without the config row
+//     const teaserBlock = document.createElement('div');
+//     teaserBlock.append(...rows.slice(1));
 
-    // Apply necessary classes and block attributes
-    teaserBlock.classList.add(...blockClasses, 'block');
-    teaserBlock.dataset.blockName = 'teaser';
-    // teaserBlock.dataset.blockStatus = 'loaded';
+//     // Apply necessary classes and block attributes
+//     teaserBlock.classList.add(...blockClasses, 'block');
+//     teaserBlock.dataset.blockName = 'teaser';
+//     // teaserBlock.dataset.blockStatus = 'loaded';
 
-    // Replace old raw block
-    teaserRaw.replaceWith(teaserBlock);
+//     // Replace old raw block
+//     teaserRaw.replaceWith(teaserBlock);
 
-    // Run the actual teaser decorator
-    await decorateTeaser(teaserBlock);
-  }));
-}
+//     // Run the actual teaser decorator
+//     await decorateTeaser(teaserBlock);
+//   }));
+// }
